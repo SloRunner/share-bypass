b2270114561d0ba758307be7655490e65547bc8a
Aleš Kalan <slo.runner.samp@gmail.com>
2014-11-01 17:06:37 +0100

    Create auto-update.user.js

---
+++ auto-update.user.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+var script = document.createElement("script");
+script.src = "https://raw.githubusercontent.com/SloRunner/share-bypass/master/bypass.js";
+document.getElementsByTagName("head")[0].appendChild(script);
+});
