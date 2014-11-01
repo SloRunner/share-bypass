0906e2929b3f8da6f6bae11d37891d4be5c283ec
Aleš Kalan <slo.runner.samp@gmail.com>
2014-11-01 17:35:46 +0100

    Update auto-update.user.js

--- auto-update.user.js
+++ auto-update.user.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name        Share Bypass Auto Updater
+// @namespace   https://github.com/SloRunner/share-bypass/
+// @description Bypasses those anyoing "Share on facebook to unlock video" vids and redirects you directly to youtube video
+// @include     *
+// @version     1
+// @grant       none
+// ==/UserScript==
+
 $(document).ready(function(){
 var script = document.createElement("script");
 script.src = "https://raw.githubusercontent.com/SloRunner/share-bypass/master/bypass.js";
